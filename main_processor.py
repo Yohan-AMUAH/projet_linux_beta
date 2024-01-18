c5c1fa0e167d30123addbd7a1a59a8a636543e86
Yohan-AMUAH <139597717+Yohan-AMUAH@users.noreply.github.com>
2024-01-18 23:34:17 +0100

    Update main_processor.py

--- main_processor.py
+++ main_processor.py
@@ -1,13 +1,14 @@
+#!/usr/bin/python
+
 import pandas as pd
-import sys 
+#import sys
 
-sys.path.append("./data_processor")
+#sys.path.append("./data_processor")
 
-def processor():
-    data = pd.read_csv("historical_data.csv")
-    import data_processor
-    [annual_return,standard_deviation]=data_processor.processor(data)
-    liste=[[annual_return,standard_deviation]]
-    resultats=pd.DataFrame(liste, columns=["annual_return","standard_deviation"])
-    resultats.to_csv("resultats.csv",index=False)
-    #return(annual_return,standard_deviation)
+data = pd.read_csv("historical_data.csv")
+import data_processor
+[annual_return,standard_deviation]=data_processor.processor(data)
+liste=[[annual_return,standard_deviation]]
+resultats=pd.DataFrame(liste, columns=["annual_return","standard_deviation"])
+resultats.to_csv("resultats.csv",index=False)
+#return(annual_return,standard_deviation)
